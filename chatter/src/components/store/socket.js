6b7771027f0dd4ac3a715aaa1604c11e343bde74
Govind Gupta <gvgupta789@gmail.com>
2024-05-18 12:22:31 +0530

    Update socket.js

--- chatter/src/components/store/socket.js
+++ chatter/src/components/store/socket.js
@@ -3,7 +3,7 @@ import { io } from 'socket.io-client';
 let origin;
 console.log(process.env.NODE_ENV,"envvvvvv");
 if( process.env.NODE_ENV === "production"){
-    origin = "http://3.106.244.65:3000" 
+    origin = "https://chatter-b.onrender.com/" 
   }
   else{
     origin = 'http://localhost:3000'
